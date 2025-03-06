5686f6c97fe813a6b0b309d73b2a649ead502f65
Rajesh <naik371998@gmail.com>
2025-03-06 19:44:16 +0530

    first commit

---
+++ src/test/java/com/gitproject/api/GitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitproject.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
